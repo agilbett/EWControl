be09be2bbf3ef930ee5b62503cf43e3771202c79
Andrew Gilbett <andrew@gilbett.net>
2018-04-22 13:25:56 +1000

    Moved away from pynput so that X is not required

--- EWcontrol.py
+++ EWcontrol.py
@@ -1,4 +1,6 @@
-from pynput import keyboard
+#!/usr/bin/python
+
+import keyboard
 from datetime import datetime
 import os
 import threading
@@ -6,6 +8,8 @@ import socket
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+ipaddress="192.168.1.195"
+devicename="EWControl"
 
 def tick():
     print('{"action":"heartbeat","requestrev":0}')
@@ -14,10 +18,10 @@ def tick():
 
 def on_press(key):
     try:
-        if key == keyboard.Key.page_down:
+        if key.name == "page down":
             print('{"action":"nextBuild","requestrev":0}')
             client_socket.send('{"action":"nextBuild","requestrev":0}\n')
-        if key == keyboard.Key.page_up:
+        if key.name == "page up":
             print('{"action":"prevBuild","requestrev":0}')
             client_socket.send('{"action":"prevBuild","requestrev":0}\n')
     except AttributeError:
@@ -43,14 +47,12 @@ t = threading.Thread(target=sched,)
 t.start()
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.1.52', 1050))
+client_socket.connect((ipaddress, 1050))
 #client_socket.connect(('localhost', 1050))
 print('{"device_type":2,"action":"connect","uid":"6BDBD0F7-DA1E-473F-8084-D63709AA10C8","device_name":"EWControl"}')
-client_socket.send('{"device_type":2,"action":"connect","uid":"06249660-262d-4cec-b9f6-f67819465ed9","device_name":"Andrews iPad"}\n')
+client_socket.send('{"device_type":2,"action":"connect","uid":"06249660-262d-4cec-b9f6-f67819465ed9","device_name":"EWControl"}\n')
 c = threading.Thread(target=clearbuf,)
 c.start()
 
+keyboard.on_press(on_press)
 
-with keyboard.Listener(
-        on_press=on_press) as listener:
-    listener.join()
